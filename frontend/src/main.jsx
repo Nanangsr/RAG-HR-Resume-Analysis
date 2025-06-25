c9dad88d4fab6f7771cf8a347ba7b2f2564f28a7
Nanangsr <nanangsafiur@gmail.com>
2025-06-25 14:31:08 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // <-- Import
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css' // Jika ada file ini
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
